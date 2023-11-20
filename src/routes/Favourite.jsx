690b39fc22a6c91838560a2fc971f3fbfd2630aa
JerryWuOn <Jon4@my.bcit.ca>
2023-11-19 22:02:31 -0800

    adjust Favourite page

--- src/routes/Favourite.jsx
+++ src/routes/Favourite.jsx
@@ -1,34 +1,101 @@
-import React from 'react';
-
+import { Link } from "react-router-dom";
+import "./HomePage.css"
 
 export default function Favourite() {
-  const handleEditFilter = () => {
-    console.log('Edit Filter button clicked');
-  };
-
   return (
     <div className="App">
-      <div>
+
+      <div className="homePageTopSection">
         <div className="navBar">
-
+        <div className="topBarBody">
+            <div>
+                <p className="logo">Cookiy</p>
+            </div>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="Favourite">Favourite</Link>
+            </div>
         </div>
+        </div>
+         <h1>Find Your Favourite Recipe</h1>
       </div>
 
-      <div className="topSection">
-        <button className="editFilterButton" onClick={handleEditFilter}>
-          Edit Filter
-        </button>
-      </div>
+<div className="bottomSection"> 
+   <div className="searchings">
+          <p className="filterFont">Your Favourites</p>
+    </div>
 
-      <div className="bottomSection">
-        <div className="searchResults">
-          <div className="cards">
-
+    <div className="searchResults">
+         <div className="cards">
+         <div className='card'>
+          <div className='cardImage'>
+            <img 
+              src=""
+              width={210}
+              height={230}/>
           </div>
+          <p className='dishName'>Recipe</p>
         </div>
-      </div>
-
+        <div className='card'>
+          <div className='cardImage'>
+            <img 
+              src=""
+              width={210}
+              height={230}/>
+          </div>
+          <p className='dishName'>Recipe</p>
+        </div>
+        <div className='card'>
+          <div className='cardImage'>
+            <img 
+              src=""
+              width={210}
+              height={230}/>
+          </div>
+          <p className='dishName'>Recipe</p>
+        </div>
+        <div className='card'>
+          <div className='cardImage'>
+            <img 
+              src=""
+              width={210}
+              height={230}/>
+          </div>
+          <p className='dishName'>Recipe</p>
+        </div>
+        <div className='card'>
+          <div className='cardImage'>
+            <img 
+              src=""
+              width={210}
+              height={230}/>
+          </div>
+          <p className='dishName'>Recipe</p>
+        </div>
+        <div className='card'>
+          <div className='cardImage'>
+            <img 
+              src=""
+              width={210}
+              height={230}/>
+          </div>
+          <p className='dishName'>Recipe</p>
+        </div>
+         </div>
+    </div>
+</div>
+<div style={{
+            width:'100%',
+            height: '80px',
+            backgroundColor: '#FEBD2D',
+            fontSize: '16px',
+            textAlign: 'center',
+            alignSelf: 'center'
+        }}>
+          <p style={{fontSize: 26}}>Created By Jerry, Kyle, Takami😆</p>
+        </div>
 
     </div>
   );
 }
+
